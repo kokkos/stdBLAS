150083157287aa84c5d776d80f770963b8c64e57
Mark Hoemmen <mhoemme@sandia.gov>
2019-09-01 18:55:23 -0600

    Initial commit of packed_layout_view header; currently empty

---
+++ include/experimental/__p1673_bits/packed_layout_view.hpp
@@ -0,0 +1,117 @@
+/*
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 2.0
+//              Copyright (2019) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software. //
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Christian R. Trott (crtrott@sandia.gov)
+//
+// ************************************************************************
+//@HEADER
+*/
+
+#ifndef LINALG_INCLUDE_EXPERIMENTAL___P1673_BITS_PACKED_LAYOUT_VIEW_HPP_
+#define LINALG_INCLUDE_EXPERIMENTAL___P1673_BITS_PACKED_LAYOUT_VIEW_HPP_
+
+#include <experimental/mdspan>
+#include "layout_triangle.hpp"
+
+namespace std {
+namespace experimental {
+inline namespace __p1673_version_0 {
+
+// TODO declarations need extents-see-returns-below defined
+
+#if 0
+template<class EltType,
+         class Extents,
+         class Layout,
+         class Accessor,
+         class Triangle,
+         class StorageOrder>
+constexpr basic_mdspan<EltType,
+  <i>extents-see-returns-below</i>,
+  layout_blas_packed<
+    Triangle,
+    StorageOrder>,
+  Accessor>
+packed_view(
+  const basic_mdspan<EltType, Extents, Layout, Accessor>& m,
+  typename basic_mdspan<EltType, Extents, Layout, Accessor>::index_type num_rows,
+  Triangle,
+  StorageOrder);
+
+         template<class EltType,
+         class Extents,
+         class Layout,
+         class Accessor,
+         class Triangle,
+         class DiagonalStorage,
+         class StorageOrder>
+constexpr basic_mdspan<const EltType,
+  <i>extents-see-returns-below</i>,
+  layout_blas_packed<
+    Triangle,
+    StorageOrder>,
+  Accessor>
+packed_view(
+  const basic_mdarray<EltType, Extents, Layout, Accessor>& m,
+  typename basic_mdarray<EltType, Extents, Layout, Accessor>::index_type num_rows,
+  Triangle,
+  StorageOrder);
+
+template<class EltType,
+         class Extents,
+         class Layout,
+         class Accessor,
+         class Triangle,
+         class DiagonalStorage,
+         class StorageOrder>
+constexpr basic_mdspan<EltType,
+  <i>extents-see-returns-below</i>,
+  layout_blas_triangular_packed<
+    Triangle,
+    StorageOrder>,
+  Accessor>
+packed_view(
+  basic_mdarray<EltType, Extents, Layout, Accessor>& m,
+  typename basic_mdarray<EltType, Extents, Layout, Accessor>::index_type num_rows,
+  Triangle,
+  StorageOrder);
+#endif // 0
+
+} // end inline namespace __p1673_version_0
+} // end namespace experimental
+} // end namespace std
+
+#endif //LINALG_INCLUDE_EXPERIMENTAL___P1673_BITS_PACKED_LAYOUT_VIEW_HPP_
