3fd7d7e6bee941b0a974f0fac9d400e2ea6141a2
Mark Hoemmen <mhoemme@sandia.gov>
2019-09-07 23:34:54 -0600

    Finish implementing matrix-vector product functions; not tested yet

--- include/experimental/__p1673_bits/blas2_matrix_vector_product.hpp
+++ include/experimental/__p1673_bits/blas2_matrix_vector_product.hpp
@@ -43,47 +43,74 @@
 #ifndef LINALG_INCLUDE_EXPERIMENTAL___P1673_BITS_BLAS2_MATRIX_VECTOR_PRODUCT_HPP_
 #define LINALG_INCLUDE_EXPERIMENTAL___P1673_BITS_BLAS2_MATRIX_VECTOR_PRODUCT_HPP_
 
+#include <complex>
+#include <type_traits>
+
 namespace std {
 namespace experimental {
 inline namespace __p1673_version_0 {
 
 // Overwriting general matrix-vector product: y := A * x
 
-template<class ExecutionPolicy,
-         class in_vector_t,
+template<class in_vector_t,
          class in_matrix_t,
          class out_vector_t>
 void matrix_vector_product(in_matrix_t A,
                            in_vector_t x,
-                           out_vector_t y);
+                           out_vector_t y)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    y(i) = 0.0;
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      y(i) += A(i,j) * x(j);
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_vector_t,
          class in_matrix_t,
          class out_vector_t>
-void matrix_vector_product(ExecutionPolicy&& exec,
+void matrix_vector_product(ExecutionPolicy&& /* exec */,
                            in_matrix_t A,
                            in_vector_t x,
-                           out_vector_t y);
+                           out_vector_t y)
+{
+  matrix_vector_product(A, x, y);
+}
 
 // Updating general matrix-vector product: z := y + A * x
 
-template<class ExecutionPolicy, class in_vector_1_t,
-         class in_matrix_t, class in_vector_2_t,
+template<class in_vector_1_t,
+         class in_matrix_t,
+         class in_vector_2_t,
          class out_vector_t>
 void matrix_vector_product(in_matrix_t A,
                            in_vector_1_t x,
                            in_vector_2_t y,
-                           out_vector_t z);
+                           out_vector_t z)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    y(i) = z(i);
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      y(i) += A(i,j) * x(j);
+    }
+  }
+}
 
-template<class ExecutionPolicy, class in_vector_1_t,
-         class in_matrix_t, class in_vector_2_t,
+template<class ExecutionPolicy,
+         class in_vector_1_t,
+         class in_matrix_t,
+         class in_vector_2_t,
          class out_vector_t>
-void matrix_vector_product(ExecutionPolicy&& exec,
+void matrix_vector_product(ExecutionPolicy&& /* exec */,
                            in_matrix_t A,
                            in_vector_1_t x,
                            in_vector_2_t y,
-                           out_vector_t z);
+                           out_vector_t z)
+{
+  matrix_vector_product(A, x, y, z);
+}
 
 // Overwriting symmetric matrix-vector product: y := A * x
 
@@ -94,18 +121,45 @@ template<class in_matrix_t,
 void symmetric_matrix_vector_product(in_matrix_t A,
                                      Triangle t,
                                      in_vector_t x,
-                                     out_vector_t y);
+                                     out_vector_t y)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    y(i) = 0.0;
+  }
+
+  if constexpr (std::is_same_v<Triangle, lower_triangle_t>) {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = j; i < A.extent(0); ++i) {
+        const auto A_ij = A(i,j);
+        y(i) += A_ij * x(j);
+        y(j) += A_ij * x(i);
+      }
+    }
+  }
+  else {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = 0; i <= j; ++i) {
+        const auto A_ij = A(i,j);
+        y(i) += A_ij * x(j);
+        y(j) += A_ij * x(i);
+      }
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_matrix_t,
          class Triangle,
          class in_vector_t,
          class out_vector_t>
-void symmetric_matrix_vector_product(ExecutionPolicy&& exec,
+void symmetric_matrix_vector_product(ExecutionPolicy&& /* exec */,
                                      in_matrix_t A,
                                      Triangle t,
                                      in_vector_t x,
-                                     out_vector_t y);
+                                     out_vector_t y)
+{
+  symmetric_matrix_vector_product(A, t, x, y);
+}
 
 // Updating symmetric matrix-vector product: z := y + A * x
 
@@ -118,7 +172,31 @@ void symmetric_matrix_vector_product(in_matrix_t A,
                                      Triangle t,
                                      in_vector_1_t x,
                                      in_vector_2_t y,
-                                     out_vector_t z);
+                                     out_vector_t z)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    z(i) = y(i);
+  }
+
+  if constexpr (std::is_same_v<Triangle, lower_triangle_t>) {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = j; i < A.extent(0); ++i) {
+        const auto A_ij = A(i,j);
+        z(i) += A_ij * x(j);
+        z(j) += A_ij * x(i);
+      }
+    }
+  }
+  else {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = 0; i <= j; ++i) {
+        const auto A_ij = A(i,j);
+        z(i) += A_ij * x(j);
+        z(j) += A_ij * x(i);
+      }
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_matrix_t,
@@ -126,12 +204,15 @@ template<class ExecutionPolicy,
          class in_vector_1_t,
          class in_vector_2_t,
          class out_vector_t>
-void symmetric_matrix_vector_product(ExecutionPolicy&& exec,
+void symmetric_matrix_vector_product(ExecutionPolicy&& /* exec */,
                                      in_matrix_t A,
                                      Triangle t,
                                      in_vector_1_t x,
                                      in_vector_2_t y,
-                                     out_vector_t z);
+                                     out_vector_t z)
+{
+  symmetric_matrix_vector_product(A, t, x, y, z);
+}
 
 // Overwriting Hermitian matrix-vector product: y := A * x
 
@@ -142,18 +223,46 @@ template<class in_matrix_t,
 void hermitian_matrix_vector_product(in_matrix_t A,
                                      Triangle t,
                                      in_vector_t x,
-                                     out_vector_t y);
+                                     out_vector_t y)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    y(i) = 0.0;
+  }
+
+  using std::conj;
+  if constexpr (std::is_same_v<Triangle, lower_triangle_t>) {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = j; i < A.extent(0); ++i) {
+        const auto A_ij = A(i,j);
+        y(i) += A_ij * x(j);
+        y(j) += conj(A_ij) * x(i);
+      }
+    }
+  }
+  else {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = 0; i <= j; ++i) {
+        const auto A_ij = A(i,j);
+        y(i) += A_ij * x(j);
+        y(j) += conj(A_ij) * x(i);
+      }
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_matrix_t,
          class Triangle,
          class in_vector_t,
          class out_vector_t>
-void hermitian_matrix_vector_product(ExecutionPolicy&& exec,
+void hermitian_matrix_vector_product(ExecutionPolicy&& /* exec */,
                                      in_matrix_t A,
                                      Triangle t,
                                      in_vector_t x,
-                                     out_vector_t y);
+                                     out_vector_t y)
+{
+  hermitian_matrix_vector_product(A, t, x, y);
+}
 
 // Updating Hermitian matrix-vector product: z := y + A * x
 
@@ -166,7 +275,32 @@ void hermitian_matrix_vector_product(in_matrix_t A,
                                      Triangle t,
                                      in_vector_1_t x,
                                      in_vector_2_t y,
-                                     out_vector_t z);
+                                     out_vector_t z)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    z(i) = y(i);
+  }
+
+  using std::conj;
+  if constexpr (std::is_same_v<Triangle, lower_triangle_t>) {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = j; i < A.extent(0); ++i) {
+        const auto A_ij = A(i,j);
+        z(i) += A_ij * x(j);
+        z(j) += conj(A_ij) * x(i);
+      }
+    }
+  }
+  else {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      for (ptrdiff_t i = 0; i <= j; ++i) {
+        const auto A_ij = A(i,j);
+        z(i) += A_ij * x(j);
+        z(j) += conj(A_ij) * x(i);
+      }
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_matrix_t,
@@ -174,12 +308,15 @@ template<class ExecutionPolicy,
          class in_vector_1_t,
          class in_vector_2_t,
          class out_vector_t>
-void hermitian_matrix_vector_product(ExecutionPolicy&& exec,
+void hermitian_matrix_vector_product(ExecutionPolicy&& /* exec */,
                                      in_matrix_t A,
                                      Triangle t,
                                      in_vector_1_t x,
                                      in_vector_2_t y,
-                                     out_vector_t z);
+                                     out_vector_t z)
+{
+  hermitian_matrix_vector_product(A, t, x, y, z);
+}
 
 // Overwriting triangular matrix-vector product: y := A * x
 
@@ -192,7 +329,37 @@ void triangular_matrix_vector_product(in_matrix_t A,
                                       Triangle t,
                                       DiagonalStorage d,
                                       in_vector_t x,
-                                      out_vector_t y);
+                                      out_vector_t y)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    y(i) = 0.0;
+  }
+  constexpr bool explicitDiagonal =
+    std::is_same_v<DiagonalStorage, explicit_diagonal_t>;
+
+  if constexpr (std::is_same_v<Triangle, lower_triangle_t>) {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      const ptrdiff_t i_lower = explicitDiagonal ? j : j + ptrdiff_t(1);
+      for (ptrdiff_t i = i_lower; i < A.extent(0); ++i) {
+        y(i) += A(i,j) * x(j);
+      }
+      if constexpr (! explicitDiagonal) {
+        y(j) += /* 1 times */ x(j);
+      }
+    }
+  }
+  else {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      const ptrdiff_t i_upper = explicitDiagonal ? j : j - ptrdiff_t(1);
+      for (ptrdiff_t i = 0; i <= i_upper; ++i) {
+        y(i) += A(i,j) * x(j);
+      }
+      if constexpr (! explicitDiagonal) {
+        y(j) += /* 1 times */ x(j);
+      }
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_matrix_t,
@@ -200,12 +367,15 @@ template<class ExecutionPolicy,
          class DiagonalStorage,
          class in_vector_t,
          class out_vector_t>
-void triangular_matrix_vector_product(ExecutionPolicy&& exec,
+void triangular_matrix_vector_product(ExecutionPolicy&& /* exec */,
                                       in_matrix_t A,
                                       Triangle t,
                                       DiagonalStorage d,
                                       in_vector_t x,
-                                      out_vector_t y);
+                                      out_vector_t y)
+{
+  triangular_matrix_vector_product(A, t, d, x, y);
+}
 
 // Updating triangular matrix-vector product: z := y + A * x
 
@@ -220,7 +390,37 @@ void triangular_matrix_vector_product(in_matrix_t A,
                                       DiagonalStorage d,
                                       in_vector_1_t x,
                                       in_vector_2_t y,
-                                      out_vector_t z);
+                                      out_vector_t z)
+{
+  for (ptrdiff_t i = 0; i < A.extent(0); ++i) {
+    z(i) = y(i);
+  }
+  constexpr bool explicitDiagonal =
+    std::is_same_v<DiagonalStorage, explicit_diagonal_t>;
+
+  if constexpr (std::is_same_v<Triangle, lower_triangle_t>) {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      const ptrdiff_t i_lower = explicitDiagonal ? j : j + ptrdiff_t(1);
+      for (ptrdiff_t i = i_lower; i < A.extent(0); ++i) {
+        z(i) += A(i,j) * x(j);
+      }
+      if constexpr (! explicitDiagonal) {
+        z(j) += /* 1 times */ x(j);
+      }
+    }
+  }
+  else {
+    for (ptrdiff_t j = 0; j < A.extent(1); ++j) {
+      const ptrdiff_t i_upper = explicitDiagonal ? j : j - ptrdiff_t(1);
+      for (ptrdiff_t i = 0; i <= i_upper; ++i) {
+        z(i) += A(i,j) * x(j);
+      }
+      if constexpr (! explicitDiagonal) {
+        z(j) += /* 1 times */ x(j);
+      }
+    }
+  }
+}
 
 template<class ExecutionPolicy,
          class in_matrix_t,
@@ -229,13 +429,16 @@ template<class ExecutionPolicy,
          class in_vector_1_t,
          class in_vector_2_t,
          class out_vector_t>
-void triangular_matrix_vector_product(ExecutionPolicy&& exec,
+void triangular_matrix_vector_product(ExecutionPolicy&& /* exec */,
                                       in_matrix_t A,
                                       Triangle t,
                                       DiagonalStorage d,
                                       in_vector_1_t x,
                                       in_vector_2_t y,
-                                      out_vector_t z);
+                                      out_vector_t z)
+{
+  triangular_matrix_vector_product(A, t, d, x, y, z);
+}
 
 } // end inline namespace __p1673_version_0
 } // end namespace experimental
