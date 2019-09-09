e41e55e9a12666f779ba105d6fad3db144a10932
Mark Hoemmen <mhoemme@sandia.gov>
2019-09-09 14:34:50 -0600

    Improve linalg_copy test

--- tests/copy.cpp
+++ tests/copy.cpp
@@ -8,6 +8,7 @@
 //   19 | #include <tbb/blocked_range.h>
 //      |          ^~~~~~~~~~~~~~~~~~~~~
 
+#include <complex>
 //#include <execution>
 #include <vector>
 #include "gtest/gtest.h"
@@ -18,7 +19,31 @@ namespace {
   using std::experimental::basic_mdspan;
   using std::experimental::linalg_copy;
 
-  TEST(BLAS1_copy, mdspan_double)
+  template<class Real>
+  struct MakeVectorValues {
+    static std::pair<Real, Real> make (const size_t k) {
+      const Real x_k = Real(k) + Real(1.0);
+      const Real y_k = Real(k) + Real(2.0);
+      return {x_k, y_k};
+    }
+  };
+
+  template<class Real>
+  struct MakeVectorValues<std::complex<Real>> {
+    static std::pair<std::complex<Real>, std::complex<Real>>
+    make (const size_t k) {
+      const std::complex<Real> x_k(Real(k) + 4.0, -Real(k) - 1.0);
+      const std::complex<Real> y_k(Real(k) + 5.0, -Real(k) - 2.0);
+      return {x_k, y_k};
+    }
+  };
+
+  template<class Scalar>
+  std::pair<Scalar, Scalar> makeVectorValues(const size_t k) {
+    return MakeVectorValues<Scalar>::make(k);
+  }
+
+  TEST(BLAS1_copy_vector, mdspan_double)
   {
     using scalar_t = double;
     using vector_t = basic_mdspan<scalar_t, extents<dynamic_extent>>;
@@ -31,22 +56,21 @@ namespace {
     vector_t y(storage.data() + vectorSize, vectorSize);
 
     for (size_t k = 0; k < vectorSize; ++k) {
-      const scalar_t x_k = scalar_t (k) + 1.0;
-      const scalar_t y_k = scalar_t (k) + 2.0;
-      x(k) = x_k;
-      y(k) = y_k;
+      const auto vals = makeVectorValues<scalar_t>(k);
+      x(k) = vals.first;
+      y(k) = vals.second;
     }
 
     linalg_copy(x, y);
     for (size_t k = 0; k < vectorSize; ++k) {
-      const scalar_t x_k = scalar_t (k) + 1.0;
+      const auto vals = makeVectorValues<scalar_t>(k);
       // Make sure the function didn't modify the input.
-      EXPECT_EQ( x(k), x_k );
-      EXPECT_EQ( y(k), x_k ); // check the output
+      EXPECT_EQ( x(k), vals.first );
+      EXPECT_EQ( y(k), vals.first ); // check the output
     }
   }
 
-  TEST(BLAS1_copy, mdspan_complex_double)
+  TEST(BLAS1_copy_vector, mdspan_complex_double)
   {
     using real_t = double;
     using scalar_t = std::complex<real_t>;
@@ -60,18 +84,17 @@ namespace {
     vector_t y(storage.data() + vectorSize, vectorSize);
 
     for (size_t k = 0; k < vectorSize; ++k) {
-      const scalar_t x_k(real_t(k) + 4.0, -real_t(k) - 1.0);
-      const scalar_t y_k(real_t(k) + 5.0, -real_t(k) - 2.0);
-      x(k) = x_k;
-      y(k) = y_k;
+      const auto vals = makeVectorValues<scalar_t>(k);
+      x(k) = vals.first;
+      y(k) = vals.second;
     }
 
     linalg_copy(x, y);
     for (size_t k = 0; k < vectorSize; ++k) {
-      const scalar_t x_k(real_t(k) + 4.0, -real_t(k) - 1.0);
+      const auto vals = makeVectorValues<scalar_t>(k);
       // Make sure the function didn't modify the input.
-      EXPECT_EQ( x(k), x_k );
-      EXPECT_EQ( y(k), x_k ); // check the output
+      EXPECT_EQ( x(k), vals.first );
+      EXPECT_EQ( y(k), vals.first ); // check the output
     }
   }
 }
