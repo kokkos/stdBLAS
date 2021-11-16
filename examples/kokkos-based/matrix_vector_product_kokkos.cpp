1ab29caa1015c0eeb2d22e951b3f9ae1989bc132
Francesco Rizzi <fnrizzi@sandia.gov>
2021-11-16 14:11:31 +0100

    blas2: customization point for mat vec

--- examples/kokkos-based/matrix_vector_product_kokkos.cpp
+++ examples/kokkos-based/matrix_vector_product_kokkos.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 
 using namespace std::experimental;
+
 int main(int argc, char* argv[]) {
   std::cout << "MatrixVectorProduct" << std::endl;
-  int N = 40;
+  int N = 10;
   int M = 20;
   Kokkos::initialize(argc,argv);
   {
@@ -13,23 +14,61 @@ int main(int argc, char* argv[]) {
     Kokkos::View<double*> y_view("Y",N);
     Kokkos::View<float**,Kokkos::LayoutRight> A_view("A",N,M);
 
-    Kokkos::deep_copy(x_view,1.0);
-    Kokkos::deep_copy(A_view,2.0);
+    {
+      // example for y = A * x
 
-    // std::experimental::mdspan a(a_ptr,N); // Requires CDAT
-    mdspan<double, extents<dynamic_extent>> x(x_view.data(),M);
-    mdspan<double, extents<dynamic_extent>> y(y_view.data(),N);
-    mdspan<float, extents<dynamic_extent,dynamic_extent>> A(A_view.data(),N,M);
+      Kokkos::deep_copy(x_view,1.0);
+      Kokkos::deep_copy(A_view,2.0);
 
-    // This forwards to KokkosKernels (https://github.com/kokkos/kokkos-kernels
-    std::experimental::linalg::matrix_vector_product(KokkosKernelsSTD::kokkos_exec<>(),A,x,y);
-    // This forwards to KokkosKernels if LINALG_ENABLE_KOKKOS_DEFAULT is ON
-    std::experimental::linalg::matrix_vector_product(A,x,y);
-    std::experimental::linalg::matrix_vector_product(std::execution::par,A,linalg::scaled(2.0,x),y);
-    // This goes to the base implementation
-    //std::experimental::linalg::matrix_vector_product(std::execution::seq,A,x,y);
+      // std::experimental::mdspan a(a_ptr,N); // Requires CDAT
+      mdspan<double, extents<dynamic_extent>> x(x_view.data(),M);
+      mdspan<double, extents<dynamic_extent>> y(y_view.data(),N);
+      mdspan<float, extents<dynamic_extent,dynamic_extent>> A(A_view.data(),N,M);
 
-    for(int i=0; i<y.extent(0); i++) printf("%i %lf\n",i,y(i));
+      // This forwards to KokkosKernels (https://github.com/kokkos/kokkos-kernels
+      linalg::matrix_vector_product(KokkosKernelsSTD::kokkos_exec<>(),A,x,y);
+      // This forwards to KokkosKernels if LINALG_ENABLE_KOKKOS_DEFAULT is ON
+      linalg::matrix_vector_product(A,x,y);
+      linalg::matrix_vector_product(std::execution::par,A,linalg::scaled(2.0,x),y);
+      // This goes to the base implementation
+      //linalg::matrix_vector_product(std::execution::seq,A,x,y);
+
+      // note that this prints 80 for each element because of the scale(2.0, x) above
+      for(int i=0; i<y.extent(0); i++) printf("%i %lf, expected = %lf\n",i,y(i), 80.);
+    }
+
+    {
+      // example for z = y + A * x
+
+      Kokkos::View<double*> z_view("Z",N);
+      Kokkos::deep_copy(x_view,1.0);
+      Kokkos::deep_copy(y_view,2.0);
+      Kokkos::deep_copy(z_view,0.0);
+      Kokkos::deep_copy(A_view,1.0);
+
+      mdspan<double, extents<dynamic_extent>> x(x_view.data(),M);
+      mdspan<double, extents<dynamic_extent>> y(y_view.data(),N);
+      mdspan<double, extents<dynamic_extent>> z(z_view.data(),N);
+      mdspan<float, extents<dynamic_extent,dynamic_extent>> A(A_view.data(),N,M);
+
+      // 1.
+      linalg::matrix_vector_product(KokkosKernelsSTD::kokkos_exec<>(),A,x,y,z);
+      // should print 22
+      for(int i=0; i<y.extent(0); i++) printf("%i %lf, expected = %lf\n",i,z(i), 22.);
+
+      // 2.
+      // scale y by 4 when passing it to kernel
+      linalg::matrix_vector_product(KokkosKernelsSTD::kokkos_exec<>(), A, x, linalg::scaled(4.,y), z);
+      // should print 28
+      for(int i=0; i<y.extent(0); i++) printf("%i %lf, expected = %lf\n",i,z(i), 28.);
+
+      // 3.
+      // scale y by 4 and x by 2 when passing it to kernel
+      linalg::matrix_vector_product(KokkosKernelsSTD::kokkos_exec<>(), A, linalg::scaled(2., x), linalg::scaled(4.,y), z);
+      // should print 48
+      for(int i=0; i<y.extent(0); i++) printf("%i %lf, expected = %lf\n",i,z(i), 48.);
+    }
+
   }
   Kokkos::finalize();
 }
