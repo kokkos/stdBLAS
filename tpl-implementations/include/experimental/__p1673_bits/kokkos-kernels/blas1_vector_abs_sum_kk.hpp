a21b0f374fc7fe976227beb88703e64fdeffe04f
Francesco Rizzi <fnrizzi@sandia.gov>
2021-12-15 09:49:17 +0100

    vector_abs_sum: kokkos impl and test

--- tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_vector_abs_sum_kk.hpp
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_vector_abs_sum_kk.hpp
@@ -4,18 +4,32 @@
 
 namespace KokkosKernelsSTD {
 
-template<class ExecSpace,
+// keeping this in mind: https://github.com/kokkos/stdBLAS/issues/122
+
+template<class ExeSpace,
          class ElementType,
 	 std::experimental::extents<>::size_type ext0,
          class Layout,
-         class Accessor,
          class Scalar>
-Scalar vector_abs_sum(kokkos_exec<ExecSpace>,
-		      std::experimental::mdspan<ElementType, std::experimental::extents<ext0>, Layout, Accessor> v,
+Scalar vector_abs_sum(kokkos_exec<ExeSpace>,
+		      std::experimental::mdspan<
+		      ElementType,
+		      std::experimental::extents<ext0>,
+		      Layout,
+		      std::experimental::default_accessor<ElementType>
+		      > x,
 		      Scalar init)
 {
-  std::cout << "vector_abs_sum: kkernels impl missing !!!\n";
-  return Scalar{};
+
+  auto x_view = Impl::mdspan_to_view(x);
+  using arithm_traits = Kokkos::Details::ArithTraits<ElementType>;
+
+  Scalar result = {};
+  Kokkos::parallel_reduce(Kokkos::RangePolicy(ExeSpace(), 0, x_view.extent(0)),
+			  KOKKOS_LAMBDA (const std::size_t i, Scalar & update) {
+			    update += arithm_traits::abs(x_view(i));
+			  }, result);
+  return result + init;
 }
 
 }
