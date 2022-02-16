242af43667d6a2725bd2cacf1c9afa75203eea07
Francesco Rizzi <fnrizzi@sandia.gov>
2022-02-16 13:18:41 +0100

    symv: kokkos: impl for udating/overwriting

--- tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas2_gemv_kk.hpp
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas2_gemv_kk.hpp
@@ -1,6 +1,6 @@
 
-#ifndef LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_MATVEC_HPP_
-#define LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_MATVEC_HPP_
+#ifndef LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_GEMV_HPP_
+#define LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_GEMV_HPP_
 
 namespace KokkosKernelsSTD {
 
@@ -219,125 +219,6 @@ matrix_vector_product(kokkos_exec<>,
 		   Impl::mdspan_to_view(y));
 }
 
-
-
-//
-// overwriting symmetric gemv: y = Ax
-//
-// for now, specialize for default_accessor
-// https://github.com/kokkos/stdBLAS/issues/122
-//
-template<class ExeSpace,
-	 class ElementType_A,
-         std::experimental::extents<>::size_type numRows_A,
-         std::experimental::extents<>::size_type numCols_A,
-         class Layout_A,
-         class Triangle,
-         class ElementType_x,
-         std::experimental::extents<>::size_type ext_x,
-         class Layout_x,
-         class ElementType_y,
-         std::experimental::extents<>::size_type ext_y,
-         class Layout_y>
-requires (Layout_A::template mapping<std::experimental::extents<numRows_A, numCols_A>>::is_always_unique())
-void symmetric_matrix_vector_product(kokkos_exec<ExeSpace> /*kexe*/,
-				     std::experimental::mdspan<
-				       ElementType_A,
-				       std::experimental::extents<numRows_A, numCols_A>,
-				       Layout_A,
-				       std::experimental::default_accessor<ElementType_A>
-				     > A,
-				     Triangle /* tr */,
-				     std::experimental::mdspan<ElementType_x,
-				       std::experimental::extents<ext_x>,
-				       Layout_x,
-				       std::experimental::default_accessor<ElementType_x>
-				     > x,
-				     std::experimental::mdspan<ElementType_y,
-				       std::experimental::extents<ext_y>,
-				       Layout_y,
-				       std::experimental::default_accessor<ElementType_y>
-				     > y)
-{
-
-  // constraints
-  static_assert(A.rank() == 2);
-  static_assert(x.rank() == 1);
-  static_assert(y.rank() == 1);
-
-  // preconditions
-  if ( A.extent(0) != A.extent(1) ){
-    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != A.extent(1) ");
-  }
-  if ( A.extent(1) != x.extent(0) ){
-    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(1) != x.extent(0) ");
-  }
-  if ( A.extent(0) != y.extent(0) ){
-    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != y.extent(0) ");
-  }
-
-  auto A_view = Impl::mdspan_to_view(A);
-  auto x_view = Impl::mdspan_to_view(x);
-  auto y_view = Impl::mdspan_to_view(y);
-
-  auto ex = ExeSpace();
-
-  // FRIZZI: improve things and maybe fuse par_fors
-  if constexpr (std::is_same_v<Triangle, std::experimental::linalg::upper_triangle_t>)
-  {
-
-  // this print is detected in the tests
-#if defined LINALG_ENABLE_TESTS
-  std::cout << "overwriting_symmetric_matrix_vector_product_upper: kokkos impl\n";
-#endif
-
-    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
-			 KOKKOS_LAMBDA (const std::size_t & i){
-			   typename decltype(y_view)::value_type y_i = {};
-			   for (std::size_t j = i; j < A_view.extent(1); ++j) {
-			     y_i += A_view(i,j) * x_view(j);
-			   }
-			   y_view(i) = y_i;
-			 });
-
-    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
-			 KOKKOS_LAMBDA (const std::size_t & i){
-			   typename decltype(y_view)::value_type lsum = {};
-			   for (std::size_t j = 0; j < i; ++j) {
-			     lsum += A_view(j,i) * x_view(j);
-			   }
-			   y_view(i) += lsum;
-			 });
-    ex.fence();
-  }
-  else{
-
-  // this print is detected in the tests
-#if defined LINALG_ENABLE_TESTS
-  std::cout << "overwriting_symmetric_matrix_vector_product_lower: kokkos impl\n";
-#endif
-
-    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
-			 KOKKOS_LAMBDA (const std::size_t & i){
-			   typename decltype(y_view)::value_type y_i = {};
-			   for (std::size_t j = 0; j <= i; ++j) {
-			     y_i += A_view(i,j) * x_view(j);
-			   }
-			   y_view(i) = y_i;
-			 });
-
-    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
-			 KOKKOS_LAMBDA (const std::size_t & i){
-			   typename decltype(y_view)::value_type lsum = {};
-			   for (std::size_t j = i+1; j < A.extent(1); ++j) {
-			     lsum += A_view(j,i) * x_view(j);
-			   }
-			   y_view(i) += lsum;
-			 });
-    ex.fence();
-  }
-
-}
 
 } // namespace KokkosKernelsSTD
 #endif
---
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas2_symv_kk.hpp
@@ -0,0 +1,261 @@
+
+#ifndef LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_SYMV_HPP_
+#define LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_SYMV_HPP_
+
+namespace KokkosKernelsSTD {
+
+//
+// overwriting symmetric gemv: y = Ax
+//
+// for now, specialize for default_accessor
+// https://github.com/kokkos/stdBLAS/issues/122
+//
+template<class ExeSpace,
+	 class ElementType_A,
+         std::experimental::extents<>::size_type numRows_A,
+         std::experimental::extents<>::size_type numCols_A,
+         class Layout_A,
+         class Triangle,
+         class ElementType_x,
+         std::experimental::extents<>::size_type ext_x,
+         class Layout_x,
+         class ElementType_y,
+         std::experimental::extents<>::size_type ext_y,
+         class Layout_y>
+requires (Layout_A::template mapping<std::experimental::extents<numRows_A, numCols_A>>::is_always_unique())
+void symmetric_matrix_vector_product(kokkos_exec<ExeSpace> /*kexe*/,
+				     std::experimental::mdspan<
+				       ElementType_A,
+				       std::experimental::extents<numRows_A, numCols_A>,
+				       Layout_A,
+				       std::experimental::default_accessor<ElementType_A>
+				     > A,
+				     Triangle /* tr */,
+				     std::experimental::mdspan<ElementType_x,
+				       std::experimental::extents<ext_x>,
+				       Layout_x,
+				       std::experimental::default_accessor<ElementType_x>
+				     > x,
+				     std::experimental::mdspan<ElementType_y,
+				       std::experimental::extents<ext_y>,
+				       Layout_y,
+				       std::experimental::default_accessor<ElementType_y>
+				     > y)
+{
+
+  // constraints
+  static_assert(A.rank() == 2);
+  static_assert(x.rank() == 1);
+  static_assert(y.rank() == 1);
+
+  // preconditions
+  if ( A.extent(0) != A.extent(1) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != A.extent(1) ");
+  }
+  if ( A.extent(1) != x.extent(0) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(1) != x.extent(0) ");
+  }
+  if ( A.extent(0) != y.extent(0) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != y.extent(0) ");
+  }
+
+  auto A_view = Impl::mdspan_to_view(A);
+  auto x_view = Impl::mdspan_to_view(x);
+  auto y_view = Impl::mdspan_to_view(y);
+
+  auto ex = ExeSpace();
+
+  // FRIZZI: improve things and maybe fuse par_fors
+  if constexpr (std::is_same_v<Triangle, std::experimental::linalg::upper_triangle_t>)
+  {
+
+  // this print is detected in the tests
+#if defined LINALG_ENABLE_TESTS
+  std::cout << "overwriting_symmetric_matrix_vector_product_upper: kokkos impl\n";
+#endif
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = i; j < A_view.extent(1); ++j) {
+			     lsum += A_view(i,j) * x_view(j);
+			   }
+			   y_view(i) = lsum;
+			 });
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = 0; j < i; ++j) {
+			     lsum += A_view(j,i) * x_view(j);
+			   }
+			   // note the +=
+			   y_view(i) += lsum;
+			 });
+    ex.fence();
+  }
+  else{
+
+  // this print is detected in the tests
+#if defined LINALG_ENABLE_TESTS
+  std::cout << "overwriting_symmetric_matrix_vector_product_lower: kokkos impl\n";
+#endif
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = 0; j <= i; ++j) {
+			     lsum += A_view(i,j) * x_view(j);
+			   }
+			   y_view(i) = lsum;
+			 });
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = i+1; j < A.extent(1); ++j) {
+			     lsum += A_view(j,i) * x_view(j);
+			   }
+			   // note the += here
+			   y_view(i) += lsum;
+			 });
+    ex.fence();
+  }
+
+}
+
+
+//
+// updating symmetric gemv: z = y + Ax
+//
+// for now, specialize for default_accessor
+// https://github.com/kokkos/stdBLAS/issues/122
+//
+template<class ExeSpace,
+	 class ElementType_A,
+         std::experimental::extents<>::size_type numRows_A,
+         std::experimental::extents<>::size_type numCols_A,
+         class Layout_A,
+         class Triangle,
+         class ElementType_x,
+         std::experimental::extents<>::size_type ext_x,
+         class Layout_x,
+         class ElementType_y,
+         std::experimental::extents<>::size_type ext_y,
+	 class Layout_y,
+         class ElementType_z,
+         std::experimental::extents<>::size_type ext_z,
+         class Layout_z>
+requires (Layout_A::template mapping<std::experimental::extents<numRows_A, numCols_A>>::is_always_unique())
+void symmetric_matrix_vector_product(kokkos_exec<ExeSpace> /*kexe*/,
+				     std::experimental::mdspan<
+				       ElementType_A,
+				       std::experimental::extents<numRows_A, numCols_A>,
+				       Layout_A,
+				       std::experimental::default_accessor<ElementType_A>
+				     > A,
+				     Triangle /* tr */,
+				     std::experimental::mdspan<ElementType_x,
+				       std::experimental::extents<ext_x>,
+				       Layout_x,
+				       std::experimental::default_accessor<ElementType_x>
+				     > x,
+				     std::experimental::mdspan<ElementType_y,
+				       std::experimental::extents<ext_y>,
+				       Layout_y,
+				       std::experimental::default_accessor<ElementType_y>
+				     > y,
+				     std::experimental::mdspan<ElementType_z,
+				       std::experimental::extents<ext_z>,
+				       Layout_z,
+				       std::experimental::default_accessor<ElementType_z>
+				     > z)
+{
+
+  // constraints
+  static_assert(A.rank() == 2);
+  static_assert(x.rank() == 1);
+  static_assert(y.rank() == 1);
+  static_assert(z.rank() == 1);
+
+  // preconditions
+  if ( A.extent(0) != A.extent(1) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != A.extent(1) ");
+  }
+  if ( A.extent(1) != x.extent(0) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(1) != x.extent(0) ");
+  }
+  if ( A.extent(0) != y.extent(0) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != y.extent(0) ");
+  }
+  if ( A.extent(0) != z.extent(0) ){
+    throw std::runtime_error("KokkosBlas: matrix_vector_product: A.extent(0) != z.extent(0) ");
+  }
+
+  auto A_view = Impl::mdspan_to_view(A);
+  auto x_view = Impl::mdspan_to_view(x);
+  auto y_view = Impl::mdspan_to_view(y);
+  auto z_view = Impl::mdspan_to_view(z);
+
+  auto ex = ExeSpace();
+
+  // FRIZZI: improve things and maybe fuse par_fors
+  if constexpr (std::is_same_v<Triangle, std::experimental::linalg::upper_triangle_t>)
+  {
+
+  // this print is detected in the tests
+#if defined LINALG_ENABLE_TESTS
+  std::cout << "updating_symmetric_matrix_vector_product_upper: kokkos impl\n";
+#endif
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = i; j < A_view.extent(1); ++j) {
+			     lsum += A_view(i,j) * x_view(j);
+			   }
+			   z_view(i) = lsum;
+			 });
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = 0; j < i; ++j) {
+			     lsum += A_view(j,i) * x_view(j);
+			   }
+			   z_view(i) += y_view(i) + lsum;
+			 });
+    ex.fence();
+  }
+  else{
+
+  // this print is detected in the tests
+#if defined LINALG_ENABLE_TESTS
+  std::cout << "updating_symmetric_matrix_vector_product_lower: kokkos impl\n";
+#endif
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = 0; j <= i; ++j) {
+			     lsum += A_view(i,j) * x_view(j);
+			   }
+			   z_view(i) = lsum;
+			 });
+
+    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, A_view.extent(0)),
+			 KOKKOS_LAMBDA (const std::size_t & i){
+			   typename decltype(y_view)::value_type lsum = {};
+			   for (std::size_t j = i+1; j < A.extent(1); ++j) {
+			     lsum += A_view(j,i) * x_view(j);
+			   }
+			   z_view(i) += y_view(i) + lsum;
+			 });
+    ex.fence();
+  }
+
+}
+
+
+} // namespace KokkosKernelsSTD
+#endif
