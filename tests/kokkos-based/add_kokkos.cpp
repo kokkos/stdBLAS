adbda1521f467832131ebf3af3cb176b49ef9476
Francesco Rizzi <fnrizzi@sandia.gov>
2022-04-08 08:36:03 +0200

    blas1 kokkos: add

---
+++ tests/kokkos-based/add_kokkos.cpp
@@ -0,0 +1,80 @@
+
+#include "gtest_fixtures.hpp"
+#include "helpers.hpp"
+
+namespace{
+
+template<class x_t, class y_t, class z_t>
+void add_gold_solution(x_t x, y_t y, z_t z)
+{
+  for (std::size_t i=0; i<x.extent(0); ++i){
+    z(i) = x(i) + y(i);
+  }
+}
+
+template<class x_t, class y_t, class z_t>
+void kokkos_blas1_add_test_impl(x_t x, y_t y, z_t z)
+{
+  namespace stdla = std::experimental::linalg;
+
+  using value_type = typename x_t::value_type;
+  const std::size_t extent = x.extent(0);
+
+  // copy x and y to verify they are not changed after kernel
+  auto x_preKernel = kokkostesting::create_stdvector_and_copy(x);
+  auto y_preKernel = kokkostesting::create_stdvector_and_copy(y);
+
+  // compute gold
+  std::vector<value_type> gold(extent);
+  using mdspan_t = mdspan<value_type, extents<dynamic_extent>>;
+  mdspan_t z_gold(gold.data(), extent);
+  add_gold_solution(x, y, z_gold);
+
+  stdla::add(KokkosKernelsSTD::kokkos_exec<>(), x, y, z);
+
+  if constexpr(std::is_same_v<value_type, float>){
+    for (std::size_t i=0; i<extent; ++i){
+      EXPECT_FLOAT_EQ(x(i), x_preKernel[i]);
+      EXPECT_FLOAT_EQ(y(i), y_preKernel[i]);
+      EXPECT_FLOAT_EQ(z(i), z_gold(i));
+    }
+  }
+
+  if constexpr(std::is_same_v<value_type, double>){
+    for (std::size_t i=0; i<extent; ++i){
+      EXPECT_DOUBLE_EQ(x(i), x_preKernel[i]);
+      EXPECT_DOUBLE_EQ(y(i), y_preKernel[i]);
+      EXPECT_DOUBLE_EQ(z(i), z_gold(i));
+    }
+  }
+
+  if constexpr(std::is_same_v<value_type, std::complex<double>>){
+    for (std::size_t i=0; i<extent; ++i){
+      EXPECT_TRUE(x(i) == x_preKernel[i]);
+      EXPECT_TRUE(y(i) == y_preKernel[i]);
+      EXPECT_DOUBLE_EQ(z(i).real(), z_gold[i].real());
+      EXPECT_DOUBLE_EQ(z(i).imag(), z_gold[i].imag());
+    }
+  }
+
+}
+}//end anonym namespace
+
+TEST_F(blas1_signed_float_fixture, kokkos_add)
+{
+  kokkos_blas1_add_test_impl(x, y, z);
+}
+
+TEST_F(blas1_signed_double_fixture, kokkos_add)
+{
+  kokkos_blas1_add_test_impl(x, y, z);
+}
+
+TEST_F(blas1_signed_complex_double_fixture, kokkos_add)
+{
+  using kc_t   = Kokkos::complex<double>;
+  using stdc_t = value_type;
+  if (alignof(value_type) == alignof(kc_t)){
+   kokkos_blas1_add_test_impl(x, y, z);
+  }
+}
--- tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_add_kk.hpp
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_add_kk.hpp
@@ -4,90 +4,54 @@
 
 namespace KokkosKernelsSTD {
 
-template <class ViewXType, class ViewYType, class ViewZType>
-struct MyTmpAddFunctorRank1
-{
-  ViewXType m_x;
-  ViewYType m_y;
-  ViewZType m_z;
+// keeping this in mind: https://github.com/kokkos/stdBLAS/issues/122
 
-  MyTmpAddFunctorRank1() = delete;
-  MyTmpAddFunctorRank1(const ViewXType x,
-		       const ViewYType y,
-		       const ViewZType z)
-    : m_x(x), m_y(y), m_z(z){}
-
-  KOKKOS_INLINE_FUNCTION
-  void operator()(std::size_t i) const{
-    m_z(i) = m_x(i) + m_y(i);
-  }
-};
-
-template <class ViewXType, class ViewYType, class ViewZType>
-struct MyTmpAddFunctorRank2
-{
-  const std::size_t m_numCols;
-  ViewXType m_x;
-  ViewYType m_y;
-  ViewZType m_z;
-
-  MyTmpAddFunctorRank2() = delete;
-  MyTmpAddFunctorRank2(const ViewXType x,
-		  const ViewYType y,
-		  const ViewZType z)
-    : m_numCols(x.extent(1)), m_x(x), m_y(y), m_z(z){}
-
-  KOKKOS_INLINE_FUNCTION
-  void operator()(std::size_t i) const
-  {
-    for (std::size_t k = 0; k < m_numCols; ++k)
-    {
-      m_z(i,k) = m_x(i,k) + m_y(i,k);
-    }
-  }
-};
-
-template<class ElementType_x,
+template<class ExeSpace,
+   class ElementType_x,
          std::experimental::extents<>::size_type ... ext_x,
          class Layout_x,
-         class Accessor_x,
          class ElementType_y,
          std::experimental::extents<>::size_type ... ext_y,
          class Layout_y,
-         class Accessor_y,
          class ElementType_z,
          std::experimental::extents<>::size_type ... ext_z,
-         class Layout_z,
-         class Accessor_z>
+         class Layout_z>
   requires (sizeof...(ext_x) == sizeof...(ext_y) && sizeof...(ext_x) == sizeof...(ext_z))
-void add(
-  kokkos_exec<>,
-  std::experimental::mdspan<ElementType_x, std::experimental::extents<ext_x ...>, Layout_x, Accessor_x> x,
-  std::experimental::mdspan<ElementType_y, std::experimental::extents<ext_y ...>, Layout_y, Accessor_y> y,
-  std::experimental::mdspan<ElementType_z, std::experimental::extents<ext_z ...>, Layout_z, Accessor_z> z)
+void add(kokkos_exec<ExeSpace>,
+   std::experimental::mdspan<
+     ElementType_x,
+     std::experimental::extents<ext_x ...>,
+     Layout_x,
+     std::experimental::default_accessor<ElementType_x>
+   > x,
+   std::experimental::mdspan<
+     ElementType_y,
+     std::experimental::extents<ext_y ...>,
+     Layout_y,
+     std::experimental::default_accessor<ElementType_y>
+   > y,
+   std::experimental::mdspan<
+     ElementType_z,
+     std::experimental::extents<ext_z ...>,
+     Layout_z,
+     std::experimental::default_accessor<ElementType_z>
+   > z)
 {
+#if defined KOKKOS_STDBLAS_ENABLE_TESTS
+  std::cout << "add: kokkos impl\n";
+#endif
+
   static_assert(z.rank() <= 2);
 
   auto x_view = Impl::mdspan_to_view(x);
-  using x_view_type = decltype(x_view);
-
   auto y_view = Impl::mdspan_to_view(y);
-  using y_view_type = decltype(y_view);
-
   auto z_view = Impl::mdspan_to_view(z);
-  using z_view_type = decltype(z_view);
 
-  // change this after adding the correct impl to KK
-  if constexpr (z.rank() == 1) {
-    using func_t = MyTmpAddFunctorRank1<x_view_type, y_view_type, z_view_type>;
-    func_t F(x_view, y_view, z_view);
-    Kokkos::parallel_for("stdBLAS::KokkosKernelsSTD_add_rank_1", x_view.extent(0), F);
-  }
-  else if constexpr (z.rank() == 2) {
-    using func_t = MyTmpAddFunctorRank2<x_view_type, y_view_type, z_view_type>;
-    func_t F(x_view, y_view, z_view);
-    Kokkos::parallel_for("stdBLAS::KokkosKernelsSTD_add_rank_2", x_view.extent(0), F);
-  }
+  const auto alpha = static_cast<ElementType_x>(1);
+  const auto beta  = static_cast<ElementType_y>(1);
+  const auto zero  = static_cast<ElementType_z>(0);
+
+  KokkosBlas::update(alpha, x_view, beta, y_view, zero, z_view);
 }
 
 }
