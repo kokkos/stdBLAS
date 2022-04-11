540e6906bfa3f0196aff47893a3c1e83b5c9f1d8
Francesco Rizzi <fnrizzi@sandia.gov>
2022-04-11 11:14:32 +0200

    kokkos tests: add test for mdspan_to_view

--- tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/mdspan_to_view_mapper_kk.hpp
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/mdspan_to_view_mapper_kk.hpp
@@ -5,12 +5,12 @@
 namespace KokkosKernelsSTD {
 namespace Impl {
 
-template<class T> struct is_complex : std::false_type{};
-template<> struct is_complex<std::complex<float>> : std::true_type{};
-template<> struct is_complex<std::complex<double>> : std::true_type{};
-template<> struct is_complex<std::complex<long double>> : std::true_type{};
+// template<class T> struct is_complex : std::false_type{};
+// template<> struct is_complex<std::complex<float>> : std::true_type{};
+// template<> struct is_complex<std::complex<double>> : std::true_type{};
+// template<> struct is_complex<std::complex<long double>> : std::true_type{};
 
-template<class T> inline constexpr bool is_complex_v = is_complex<T>::value;
+// template<class T> inline constexpr bool is_complex_v = is_complex<T>::value;
 
 
 // These are only minimal mappers from mdspan to View without safety checks.
@@ -29,71 +29,54 @@ struct LayoutMapper<std::experimental::layout_right> {
   using type = Kokkos::LayoutRight;
 };
 
+template<class T>
+constexpr void static_assert_complex_alignment(const T* /* p */)
+{
+  using kc_t = Kokkos::complex<typename T::value_type>;
+  static_assert(alignof(T) == alignof(kc_t),
+		"KokkosBlas: mdspan_to_view: mismatching alignment of std::complex<> and Kokkos::complex<> which prevents interoperability. By default, Kokkos::complex<T> is aligned as 2*sizeof(T) but std::complex<T> is aligned as sizeof(T). To match the alignment and allow correct conversion, you must rebuild Kokkos with KOKKOS_ENABLE_COMPLEX_ALIGN undefined.");
+}
+
+//
+// to_kokkos_pointer
+//
+template<class ValueType>
+ValueType* to_kokkos_pointer(ValueType* p) {
+  return p;
+}
+
+template<class RealType>
+auto to_kokkos_pointer(std::complex<RealType>* p) {
+  static_assert_complex_alignment(p);
+  return reinterpret_cast<Kokkos::complex<RealType>*>(p);
+}
+
+template<class RealType>
+auto to_kokkos_pointer(const std::complex<RealType>* p) {
+  static_assert_complex_alignment(p);
+  return reinterpret_cast<const Kokkos::complex<RealType>*>(p);
+}
+
+//
+// mdspan_to_view
+//
 template<class MDSpan>
-std::enable_if_t<
-  MDSpan::rank()==1
-  && !is_complex_v<typename MDSpan::element_type>,
-  Kokkos::View<typename MDSpan::element_type*>
-  >
-mdspan_to_view(const MDSpan& a) {
-  return Kokkos::View<typename MDSpan::element_type*>(a.data(), a.extent(0));
+requires (MDSpan::rank()==1)
+auto mdspan_to_view(const MDSpan& a) {
+  auto kokkos_p = to_kokkos_pointer(a.data());
+  return Kokkos::View<decltype(kokkos_p)>(kokkos_p, a.extent(0));
 }
 
 template<class MDSpan>
-std::enable_if_t<
-  MDSpan::rank()==1
-  && is_complex_v<typename MDSpan::element_type>,
-  Kokkos::View< Kokkos::complex<typename MDSpan::element_type::value_type>* >
-  >
-mdspan_to_view(const MDSpan& a)
+requires (MDSpan::rank()==2)
+auto mdspan_to_view(const MDSpan& a)
 {
-  using mdspan_v_type = typename MDSpan::element_type::value_type;
-  using kc_t = Kokkos::complex<mdspan_v_type>;
-  if (alignof(mdspan_v_type) != alignof(kc_t)){
-    throw std::runtime_error("KokkosBlas: mdspan_to_view: mismatching alignment of std::complex<> and Kokkos::complex<> which prevents interoperability. By default, Kokkos::complex<T> is aligned as 2*sizeof(T) but std::complex<T> is aligned as sizeof(T). To match the alignement and allow correct conversion, you can rebuild Kokkos undefining KOKKOS_ENABLE_COMPLEX_ALIGN.");
-  }
-
-  using view_type = Kokkos::View< kc_t* >;
-  return view_type(reinterpret_cast<kc_t*>(a.data()), a.extent(0));
-}
-
-template<class MDSpan>
-std::enable_if_t<
-  MDSpan::rank()==2
-  && !is_complex_v<typename MDSpan::element_type>,
-  Kokkos::View<
-    typename MDSpan::element_type **,
-    typename LayoutMapper<typename MDSpan::layout_type>::type
-    >
-  >
-mdspan_to_view(const MDSpan& a)
-{
+  auto kokkos_p = to_kokkos_pointer(a.data());
   using view_type = Kokkos::View<
-    typename MDSpan::element_type **,
+    decltype(kokkos_p)*,
     typename LayoutMapper<typename MDSpan::layout_type>::type
     >;
-  return view_type(a.data(), a.extent(0), a.extent(1));
-}
-
-template<class MDSpan>
-std::enable_if_t<
-  MDSpan::rank()==2
-  && is_complex_v<typename MDSpan::element_type>,
-  Kokkos::View<
-    Kokkos::complex< typename MDSpan::element_type::value_type> **,
-    typename LayoutMapper<typename MDSpan::layout_type>::type
-    >
-  >
-mdspan_to_view(const MDSpan& a)
-{
-  using mdspan_v_type = typename MDSpan::element_type::value_type;
-  using kc_t = Kokkos::complex<mdspan_v_type>;
-  if (alignof(mdspan_v_type) != alignof(kc_t)){
-    throw std::runtime_error("KokkosBlas: mdspan_to_view: mismatching alignment of std::complex<> and Kokkos::complex<> which prevents interoperability. By default, Kokkos::complex<T> is aligned as 2*sizeof(T) but std::complex<T> is aligned as sizeof(T). To match the alignement and allow correct conversion, you can rebuild Kokkos undefining KOKKOS_ENABLE_COMPLEX_ALIGN.");
-  }
-
-  using view_type = Kokkos::View<kc_t**, typename LayoutMapper<typename MDSpan::layout_type>::type>;
-  return view_type(reinterpret_cast<kc_t*>(a.data()), a.extent(0), a.extent(1));
+  return view_type(kokkos_p, a.extent(0), a.extent(1));
 }
 
 } // namespace Impl
