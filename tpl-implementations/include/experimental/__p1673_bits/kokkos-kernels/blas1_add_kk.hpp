c5d2218c8b37463505569bc652c805f9173e70bf
Francesco Rizzi <fnrizzi@sandia.gov>
2022-04-14 11:27:42 +0200

    add: kokkos impl

--- tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_add_kk.hpp
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_add_kk.hpp
@@ -2,7 +2,32 @@
 #ifndef LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_ADD_HPP_
 #define LINALG_TPLIMPLEMENTATIONS_INCLUDE_EXPERIMENTAL___P1673_BITS_KOKKOSKERNELS_ADD_HPP_
 
+#include "signal_kokkos_impl_called.hpp"
+
 namespace KokkosKernelsSTD {
+
+namespace add_impl{
+
+template <class size_type>
+KOKKOS_INLINE_FUNCTION
+constexpr bool static_extent_match(size_type extent1, size_type extent2)
+{
+  return extent1 == std::experimental::dynamic_extent ||
+         extent2 == std::experimental::dynamic_extent ||
+         extent1 == extent2;
+}
+
+template <class F, class T, T... Is>
+void repeat_impl(F&& f, std::integer_sequence<T, Is...>){
+  ( f(std::integral_constant<T, Is>{}), ... );
+}
+
+template <int N, class F>
+void repeat(F&& f){
+  repeat_impl(f, std::make_integer_sequence<int, N>{});
+}
+
+} // namespace add_impl
 
 // keeping this in mind: https://github.com/kokkos/stdBLAS/issues/122
 
@@ -18,38 +43,57 @@ template<class ExeSpace,
          class Layout_z>
   requires (sizeof...(ext_x) == sizeof...(ext_y) && sizeof...(ext_x) == sizeof...(ext_z))
 void add(kokkos_exec<ExeSpace>,
-   std::experimental::mdspan<
-     ElementType_x,
-     std::experimental::extents<ext_x ...>,
-     Layout_x,
-     std::experimental::default_accessor<ElementType_x>
-   > x,
-   std::experimental::mdspan<
-     ElementType_y,
-     std::experimental::extents<ext_y ...>,
-     Layout_y,
-     std::experimental::default_accessor<ElementType_y>
-   > y,
-   std::experimental::mdspan<
-     ElementType_z,
-     std::experimental::extents<ext_z ...>,
-     Layout_z,
-     std::experimental::default_accessor<ElementType_z>
-   > z)
+	 std::experimental::mdspan<
+	   ElementType_x,
+	   std::experimental::extents<ext_x ...>,
+	   Layout_x,
+	   std::experimental::default_accessor<ElementType_x>
+	 > x,
+	 std::experimental::mdspan<
+	   ElementType_y,
+	   std::experimental::extents<ext_y ...>,
+	   Layout_y,
+	   std::experimental::default_accessor<ElementType_y>
+	 > y,
+	 std::experimental::mdspan<
+	   ElementType_z,
+	   std::experimental::extents<ext_z ...>,
+	   Layout_z,
+	   std::experimental::default_accessor<ElementType_z>
+	 > z)
 {
-#if defined KOKKOS_STDBLAS_ENABLE_TESTS
-  std::cout << "add: kokkos impl\n";
-#endif
-
   static_assert(z.rank() <= 2);
+
+  // P1673 preconditions
+  add_impl::repeat<x.rank()>
+    ([=](int r){
+      if ( x.extent(r) != y.extent(r) ){
+	throw std::runtime_error("KokkosBlas: add: x.extent(r) != y.extent(r) for r="
+				 + std::to_string(r));
+      }
+      if ( y.extent(r) != z.extent(r) ){
+	throw std::runtime_error("KokkosBlas: add: y.extent(r) != z.extent(r) for r = "
+				 + std::to_string(r));
+      }
+    });
+
+  // P1673 mandates
+  add_impl::repeat<x.rank()>
+    ([=](int r){
+      add_impl::static_extent_match(x.static_extent(r), z.static_extent(r));
+      add_impl::static_extent_match(y.static_extent(r), z.static_extent(r));
+      add_impl::static_extent_match(x.static_extent(r), y.static_extent(r));
+    });
+
+  Impl::signal_kokkos_impl_called("add");
 
   auto x_view = Impl::mdspan_to_view(x);
   auto y_view = Impl::mdspan_to_view(y);
   auto z_view = Impl::mdspan_to_view(z);
 
-  const auto alpha = static_cast<ElementType_x>(1);
-  const auto beta  = static_cast<ElementType_y>(1);
-  const auto zero  = static_cast<ElementType_z>(0);
+  const auto alpha = static_cast<typename decltype(x_view)::non_const_value_type>(1);
+  const auto beta  = static_cast<typename decltype(y_view)::non_const_value_type>(1);
+  const auto zero  = static_cast<typename decltype(z_view)::non_const_value_type>(0);
 
   KokkosBlas::update(alpha, x_view, beta, y_view, zero, z_view);
 }
