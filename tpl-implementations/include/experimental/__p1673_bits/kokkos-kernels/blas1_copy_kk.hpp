e27a1785e1f9508b89b74a57c787df0775b000d7
Francesco Rizzi <fnrizzi@sandia.gov>
2022-04-25 08:59:07 +0200

    copy: use deeep_copy when possible

--- tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_copy_kk.hpp
+++ tpl-implementations/include/experimental/__p1673_bits/kokkos-kernels/blas1_copy_kk.hpp
@@ -32,26 +32,34 @@ requires ((sizeof...(ext_x) == sizeof...(ext_y)) && (x.rank() <=2))
 
   auto x_view = Impl::mdspan_to_view(x);
   auto y_view = Impl::mdspan_to_view(y);
-
   auto ex = ExeSpace();
-  if constexpr(x.rank()==1){
-    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, x_view.extent(0)),
-			 KOKKOS_LAMBDA (const std::size_t i){
-			   y_view(i) = x_view(i);
-			 });
+
+  if constexpr(std::is_same_v<typename decltype(x_view)::array_layout, typename decltype(y_view)::array_layout>) {
+    Kokkos::deep_copy(ex, y, x);
+  } else {
+
+    if constexpr(x.rank()==1){
+      Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, x_view.extent(0)),
+			   KOKKOS_LAMBDA (const std::size_t i){
+			     y_view(i) = x_view(i);
+			   });
+    }
+
+    else{
+      Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, x_view.extent(0)),
+			   KOKKOS_LAMBDA (const std::size_t i){
+			     for (std::size_t j=0; j<x_view.extent(1); ++j){
+			       y_view(i,j) = x_view(i,j);
+			     }
+			   });
+    }
   }
 
-  else{
-    Kokkos::parallel_for(Kokkos::RangePolicy(ex, 0, x_view.extent(0)),
-			 KOKKOS_LAMBDA (const std::size_t i){
-			   for (std::size_t j=0; j<x_view.extent(1); ++j){
-			     y_view(i,j) = x_view(i,j);
-			   }
-			 });
-  }
-
-  //fence message when using latest kokkos:
+  // need to fence even for deep_copy case since passing
+  // ex to it makes deep_copy potentially non-blocking
+  // https://github.com/kokkos/kokkos/wiki/Kokkos%3A%3Adeep_copy
   ex.fence();
+  //fence message when using latest kokkos:
   // ex.fence("KokkosStdBlas::copy: fence after operation");
 }
 
