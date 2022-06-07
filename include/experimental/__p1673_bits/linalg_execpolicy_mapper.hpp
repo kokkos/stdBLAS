a68a846b69270c20a05ecc40be3a82c9d5bfeb5b
Mark Hoemmen <mhoemmen@nvidia.com>
2022-06-07 12:45:33 -0600

    Fix GCC 9 build
    Don't unconditionally include <execution>.
    
    Fixes #231.

--- include/experimental/__p1673_bits/linalg_execpolicy_mapper.hpp
+++ include/experimental/__p1673_bits/linalg_execpolicy_mapper.hpp
@@ -1,4 +1,9 @@
+// For GCC 9 (< 10), <execution> unconditionally includes a TBB header file.
+// If GCC < 10 was not built with TBB support, this causes a build error.
+#if ! defined(__GNUC__) || __GNUC__ > 9
 #include <execution>
+#endif
+
 #include <type_traits>
 
 namespace std {
