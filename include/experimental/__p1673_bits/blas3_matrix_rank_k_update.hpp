4c9089a1d18086f217186e57b004e40407ed72ce
Mark Hoemmen <mhoemmen@nvidia.com>
2024-01-13 03:50:39 -0700

    Fix symmetric_matrix_rank_k_update
    1. Fix ambiguous overload for the no-scalar case
    2. Fix the algorithm (so it gives the right answer
       for the no-scalar case; the with-scalar case
       was already fixed in a previous commit)

--- include/experimental/__p1673_bits/blas3_matrix_rank_k_update.hpp
+++ include/experimental/__p1673_bits/blas3_matrix_rank_k_update.hpp
@@ -149,7 +149,6 @@ void symmetric_matrix_rank_k_update(
   std::experimental::mdspan<ElementType_C, std::experimental::extents<SizeType_C, numRows_C, numCols_C>, Layout_C, Accessor_C> C,
   Triangle /* t */)
 {
-  constexpr bool lower_tri = std::is_same_v<Triangle, lower_triangle_t>;
   using size_type = std::common_type_t<SizeType_A, SizeType_C>;
 
   if constexpr (std::is_same_v<Triangle, upper_triangle_t>) {
@@ -231,8 +230,10 @@ MDSPAN_TEMPLATE_REQUIRES(
   class Accessor_C,
   class Triangle,
   /* requires */ (
-    std::is_same_v<Triangle, lower_triangle_t> ||
-    std::is_same_v<Triangle, upper_triangle_t>
+    (! std::is_execution_policy_v<ScaleFactorType> &&
+     ! std::is_same_v<std::experimental::linalg::impl::default_exec_t, ScaleFactorType>) &&
+    (std::is_same_v<Triangle, lower_triangle_t> ||
+     std::is_same_v<Triangle, upper_triangle_t>)
   )
 )
 void symmetric_matrix_rank_k_update(
@@ -245,7 +246,7 @@ void symmetric_matrix_rank_k_update(
 }
 
 
-// Rank-k update of a symmetric matrix without scaling factor
+// Rank-k update of a symmetric matrix without scaling factor alpha
 
 MDSPAN_TEMPLATE_REQUIRES(
   class ElementType_A,
@@ -263,20 +264,38 @@ MDSPAN_TEMPLATE_REQUIRES(
   )
 )
 void symmetric_matrix_rank_k_update(
-  std::experimental::linalg::impl::inline_exec_t&& /*exec*/,
+  std::experimental::linalg::impl::inline_exec_t&& /* exec */,
   std::experimental::mdspan<ElementType_A, std::experimental::extents<SizeType_A, numRows_A, numCols_A>, Layout_A, Accessor_A> A,
   std::experimental::mdspan<ElementType_C, std::experimental::extents<SizeType_C, numRows_C, numCols_C>, Layout_C, Accessor_C> C,
   Triangle /* t */)
 {
-  constexpr bool lower_tri = std::is_same_v<Triangle, lower_triangle_t>;
-  using size_type = ::std::common_type_t<SizeType_A, SizeType_C>;
+  using size_type = std::common_type_t<SizeType_A, SizeType_C>;
 
-  for (size_type j = 0; j < C.extent(1); ++j) {
-    const size_type i_lower = lower_tri ? j : size_type(0);
-    const size_type i_upper = lower_tri ? C.extent(0) : j+1;
-    for (size_type i = i_lower; i < i_upper; ++i) {
-      for (size_type k = 0; k < A.extent(1); ++k) {
-        C(i,j) += A(i,k)*A(j,k);
+  if constexpr (std::is_same_v<Triangle, upper_triangle_t>) {
+    // [A_00  A_01  A_02]   [A_00            ]
+    // [      A_11  A_12] * [A_01  A_11      ]
+    // [            A_22]   [A_02  A_12  A_22]
+    for (size_type row = 0; row < C.extent(0); ++row) {
+      for (size_type col = 0; col < C.extent(1); ++col) {
+        const size_type lower = row < col ? col : row; // max
+        const size_type upper = A.extent(0);
+        for (size_type k = lower; k < upper; ++k) {
+          C(row, col) += A(row, k) * A(col, k);
+        }
+      }
+    }
+  }
+  else {
+    // [A_00            ]   [A_00  A_10  A_20]
+    // [A_10  A_11      ] * [      A_11  A_21]
+    // [A_20  A_21  A_22]   [            A_22]
+    for (size_type row = 0; row < C.extent(0); ++row) {
+      for (size_type col = 0; col < C.extent(1); ++col) {
+        const size_type lower = 0;
+        const size_type upper = row < col ? row : col; // min
+        for (size_type k = lower; k < upper; ++k) {
+          C(row, col) += A(row, k) * A(col, k);
+        }
       }
     }
   }
@@ -366,7 +385,6 @@ void hermitian_matrix_rank_k_update(
   std::experimental::mdspan<ElementType_C, std::experimental::extents<SizeType_C, numRows_C, numCols_C>, Layout_C, Accessor_C> C,
   Triangle /* t */)
 {
-  constexpr bool lower_tri = std::is_same_v<Triangle, lower_triangle_t>;
   using size_type = std::common_type_t<SizeType_A, SizeType_C>;
 
   if constexpr (std::is_same_v<Triangle, upper_triangle_t>) {
@@ -486,7 +504,6 @@ void hermitian_matrix_rank_k_update(
   std::experimental::mdspan<ElementType_C, std::experimental::extents<SizeType_C, numRows_C, numCols_C>, Layout_C, Accessor_C> C,
   Triangle /* t */)
 {
-  constexpr bool lower_tri = std::is_same_v<Triangle, lower_triangle_t>;
   using size_type = std::common_type_t<SizeType_A, SizeType_C>;
 
   if constexpr (std::is_same_v<Triangle, upper_triangle_t>) {
@@ -576,7 +593,6 @@ void hermitian_matrix_rank_k_update(
 {
   hermitian_matrix_rank_k_update(std::experimental::linalg::impl::default_exec_t(), A, C, t);
 }
-
 
 } // end namespace linalg
 } // end inline namespace __p1673_version_0
