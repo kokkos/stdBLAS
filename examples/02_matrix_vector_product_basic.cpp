fe489cf60777be96c8acadaa2d6ce41a7a0bc106
Mark Hoemmen <mhoemmen@nvidia.com>
2025-01-08 17:26:18 -0700

    Start updating license headers

--- examples/02_matrix_vector_product_basic.cpp
+++ examples/02_matrix_vector_product_basic.cpp
@@ -1,3 +1,20 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// ************************************************************************
+//@HEADER
+
 // Examples currently use parentheses (e.g., A(i,j))
 // for the array access operator,
 // instead of square brackets (e.g., A[i,j]).
